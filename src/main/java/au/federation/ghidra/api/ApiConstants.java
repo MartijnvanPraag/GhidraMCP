14ef4119e047166bb875583c1e3867baa6c01211
MartijnvanPraag <mvanpraag@outlook.com>
2025-10-11 14:32:29 +1100

    Version update

--- src/main/java/au/federation/ghidra/api/ApiConstants.java
+++ src/main/java/au/federation/ghidra/api/ApiConstants.java
@@ -1,7 +1,7 @@
 package au.federation.ghidra.api;
 
 public class ApiConstants {
-    public static final String PLUGIN_VERSION = "v1.0.0.1";
+    public static final String PLUGIN_VERSION = "v2.0.0.1";
     public static final int API_VERSION = 2005;
     public static final int DEFAULT_PORT = 8192;
     public static final int MAX_PORT_ATTEMPTS = 10;
